b1064b05c3059ba3c096c6a6196c0dd2ab8ce3ef
Daisuke <onaya.daisue629@gmail.com>
2026-05-08 23:55:01 +0900

    companiesテーブルカラム統合

--- database/migrations/2026_05_08_081627_create_companies_table.php
+++ database/migrations/2026_05_08_081627_create_companies_table.php
@@ -11,6 +11,8 @@ class CreateCompaniesTable extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('company_name');
+            $table->string('street_address')->nullable();
+            $table->string('representative_name')->nullable();
             $table->timestamps();
         });
     }
